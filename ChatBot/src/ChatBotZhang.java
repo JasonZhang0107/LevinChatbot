ddc08db11c7650c63aa51b0309ce94d78db557a9
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-10-04 12:49:58 -0400

    Jason Changes

---
+++ ChatBot/src/ChatBotZhang.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+/**
+ * A program to carry on conversations with a human user.
+ * This version:
+ * @author Jason Zhang
+ * @version Health/Fitness
+ */
+public class ChatBotZhang
+{
+	
+}
