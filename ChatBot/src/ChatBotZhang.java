52fd04b6474bec86347a9a872ae70761d593bc47
Jason <Jason@Lenovo-PC.fios-router.home>
2017-10-12 01:04:53 -0400

    Jason final changes #3

--- ChatBot/src/ChatBotZhang.java
+++ ChatBot/src/ChatBotZhang.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A program to carry on conversations with a human user.
@@ -12,20 +13,23 @@ public class ChatBotZhang
 	String response = "";
 	
 	//emotion can alter the bot's responses and can be changed as the conversation progresses
-	// emotion starts at 5, can go to as low as 0 or as high as 10
+	//the bot can have positive emotion which is hapy, or negative emotion which is sad/angry
 	int emotion = 0;
 	
 	//user name
 	String name = "";
 	
+	//user goal
+	String goal = "";
+	
+	//variable needed for later use to keep track of the string
+	String goalResponse = "";
+	
 	//question type so the bot knows how to respond
 	String questionType = "";
 	
-	//chatbot's memory
-	public String [] chatbotMemory = {};
-	
 	//List of strings that would prompt the user to switch to another chatbot
-	public String [] suggestSidneyBot = 
+	public String [] romance = 
 		{
 				"crush",
 				"romance",
@@ -33,6 +37,23 @@ public class ChatBotZhang
 				"cute",
 				"affection",
 		};
+	public String [] existence = 
+		{
+				"space",
+				"time",
+				"reality",
+				"existence",
+				"life",
+		};
+	public String [] therapy = 
+		{
+				"therapy",
+				"therapist",
+				"cure",
+				"healing",
+				"remedy",
+		};
+	
 	/*
 	 * Description: Greets the user at the beginning of the conversation
 	 * Returns: A greeting
@@ -40,7 +61,7 @@ public class ChatBotZhang
 	public String getGreeting()
 	{
 		questionType = "name";
-		return "Hello my name is Connor, what is your name?";
+		return "Hello, I am the fitness bot.\nMy name is Connor, what is your name?";
 	}
 	/*
 	 * Description: Responds to a user statement
@@ -49,8 +70,6 @@ public class ChatBotZhang
 	 */
 	public String getResponse(String statement)
 	{
-		String response = "";
-		
 		if(questionType == "name")
 		{
 			response = nameResponse(statement);
@@ -58,6 +77,34 @@ public class ChatBotZhang
 		else if(questionType == "goal")
 		{
 			response = goalResponse(statement);
+		}
+		else if(questionType == "workoutType")
+		{
+			response = workoutTypeResponse(statement);
+		}
+		else if(questionType == "end")
+		{
+			response = endResponse(statement);
+		}
+		else if(questionType == "whichBot")
+		{
+			whichBotResponse(statement);
+		}
+		return emotionalResponses(response);
+	}
+	public String emotionalResponses(String statement)
+	{
+		if(emotion < 0)
+		{
+			response = statement.toUpperCase() + "I AM SO MAD AT YOU RIGHT NOW!";
+		}
+		if(emotion > 0)
+		{
+			response = statement + "Go get them gains!!";
+		}
+		if(emotion == 0)
+		{
+			response = statement;
 		}
 		return response;
 	}
@@ -115,23 +162,31 @@ public class ChatBotZhang
 			return true;
 		}
 	}
-	//takes their name in and gives a response
+	/*
+	 * Description: Ask the user for their name
+	 * Parameter: The user response represented by statement
+	 * Return: A response that uses the user's name. Prompts them for more information
+	 */
 	public String nameResponse(String statement)
 	{
 		name = statement;
-		response = "Hello " +name+ " what are your fitness goals?\nIt can be anything from losing weight to gaining muscle";
+		response = "Hello " +name+ " what are your fitness goals?\nIt can be anything from losing weight to gaining muscle.";
 		questionType = "goal";
 		
 		return response;
 	}
-	//takes in their statement looking for the words gain, gaining, lose, and losing to prompt a response associated with each specific one
+	/*
+	 * Description: Ask the user for their fitness goals
+	 * Parameter: The user response represented by statement
+	 * Return: A response to the user based on what they answered as well as prompting them with a new question
+	 */
 	public String goalResponse(String statement)
 	{
-		String goal = "";
 		//if neither gain or lose is found, it will prompt the user to give another statement
 		if(isKeywordFound(statement, "gain") == false && isKeywordFound(statement,"lose")==false && isKeywordFound(statement,"losing")==false && isKeywordFound(statement,"gaining")==false)
 		{
-			response = "Sorry man, I couldn't understand what you said. Can you tell me in simpler terms what your goal is.";
+			response = "I said what are your fitness goals. Answer me properly!";
+			emotion--;
 		}
 		else
 		{
@@ -140,28 +195,48 @@ public class ChatBotZhang
 				int positionOfWord = findKeyword(statement, "gain");
 				goal = statement.substring(positionOfWord + 5);
 				
-				response = "Alright then, so you want to gain " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan";
+				response = "Alright then, so you want to gain " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan.";
+				goalResponse = response;
+				
+				questionType = "workoutType";
+				
+				emotion++;
 			}
 			if(isKeywordFound(statement, "gaining"))
 			{
 				int positionOfWord = findKeyword(statement, "gaining");
 				goal = statement.substring(positionOfWord + 8);
 				
-				response = "Alright then, so you want to gain " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan";
+				response = "Alright then, so you want to gain " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan.";
+				goalResponse = response;
+				
+				questionType = "workoutType";
+				
+				emotion++;
 			}
 			if(isKeywordFound(statement, "lose"))
 			{
 				int positionOfWord = findKeyword(statement, "lose");
 				goal = statement.substring(positionOfWord + 5);
 				
-				response = "Alright then, so you want to lose " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan";
+				response = "Alright then, so you want to lose " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan.";
+				goalResponse = response;
+				
+				questionType = "workoutType";
+				
+				emotion++;
 			}
 			if(isKeywordFound(statement, "losing"))
 			{
 				int positionOfWord = findKeyword(statement, "losing");
 				goal = statement.substring(positionOfWord + 7);
 				
-				response = "Alright then, so you want to lose " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan";
+				response = "Alright then, so you want to lose " +goal+ ".\nDo you want to learn more about nutrition or perhaps a workout plan.";
+				goalResponse = response;
+				
+				questionType = "workoutType";
+				
+				emotion++;
 			}
 			//if both gain and lose is found, it will return a response incorporating both of what the user wants
 			//this if statement needs to be placed at the end to overwrite the other two is conditions are met
@@ -175,22 +250,246 @@ public class ChatBotZhang
 				
 				if(positionOfWordGain > positionOfWordLose)
 				{
-					response = "Alright then so you want to lose " +goalLose+ ".\nDo you want to learn more about nutrition or perhaps a workout plan";
+					response = "Alright then so you want to lose " +goalLose+ ".\nDo you want to learn more about nutrition or perhaps a workout plan.";
+					goalResponse = response;
+					
+					questionType = "workoutType";
+					
+					emotion++;
 				}
 				else
 				{
-					response = "Alright then so you want to gain " +goalGain+ ".\nDo you want to learn more about nutrition or perhaps a workout plan";
+					response = "Alright then so you want to gain " +goalGain+ ".\nDo you want to learn more about nutrition or perhaps a workout plan.";
+					goalResponse = response;
+					
+					questionType = "workoutType";
+					
+					emotion++;
 				}
 			}
 		}
-		questionType = "workoutType";
 		return response;
 	}
-	Public String workoutTypeResponse(statement)
+	/*
+	 * Description: Returns the user some useful information and asks if they would like to speak with another bot
+	 * Parameter: The user response represented by statement
+	 * Return: A response based on the user's answer to this question and a previous question
+	 */
+	public String workoutTypeResponse(String statement)
 	{
-		if(findKeyword(statement, "nutrition") == false || findKeyword(statement, "workout") == false)
+		if(isKeywordFound(statement, "nutrition") == false && isKeywordFound(statement, "workout") == false)
 		{
-			response = ""
+			response = "I asked if you wanted to learn about nutrition or workout plan.\nI can tell your not serious about this, are you.";
+			emotion--;
+		}
+		else
+		{
+			if(isKeywordFound(statement, "nutrition"))
+			{
+				if(isKeywordFound(goalResponse, "gain") || isKeywordFound(goalResponse, "gaining"))
+				{
+					response = "The key to gaining " +goal+ " is to eat in a caloric surplus.\nYour muscles need more calories than your maintence for growth.\nI think we've done enough talking, now its time for you to get to work or would you prefer to speak with another bot?";
+					questionType = "end";
+					
+					emotion++;
+				}
+	 			else
+	 			{
+	 				if(isKeywordFound(goalResponse, "lose") || isKeywordFound(goalResponse, "losing"))
+	 				{
+	 					response = "The key to losing " +goal+ " is to eat in a caloric deficit, when you consume less calories than you need expend, you will lose weight.\nI think we've done enough talking, now its time for you to get to work or would you prefer to speak with another bot?";
+	 					questionType = "end";
+	 					
+	 					emotion++;
+	 				}
+	 			}
+	 		}
+	 		if(isKeywordFound(statement, "workout"))
+ 			{
+ 				if(isKeywordFound(goalResponse, "gain") || isKeywordFound(goalResponse, "gaining"))
+ 				{
+ 					response = "The key to gaining " +goal+ " in a workout is strength training.\nI would suggest a full body workout for 4-5 days a week.\nI think we've done enough talking, now its time for you to get to work or would you prefer to speak with another bot?";
+ 					questionType = "end";
+ 					
+ 					emotion++;
+ 				}
+ 				else
+ 				{
+					if(isKeywordFound(goalResponse, "lose") || isKeywordFound(goalResponse, "losing"))
+ 					{
+ 						response = "The key to losing" +goal+ " for a workout plan is doing cardio.\nHIIT cardio for 20 minutes each time is highly effective.\nI think we've done enough talking, now its time for you to get to work or would you prefer to speak with another bot?";
+ 						questionType = "end";
+ 						
+ 						emotion++;
+					}
+ 				}
+	 		}
+		}
+		return response;
+	}
+	/*
+	 * Description: If user wants to talk with another bot, they are offered different options. If not, the conversation ends.
+	 * Parameter: The user response represented by statement
+	 * Return: Ask the user which bot they'd like to speak with or end the conversation with a different response
+	 */
+	public String endResponse(String statement)
+	{
+		if(isKeywordFound(statement, "talk with another bot") || isKeywordFound(statement, "talk more") || isKeywordFound(statement, "another bot") || isKeywordFound(statement, "speak"))
+		{
+			response = "Which bot would you like to speak with?\nSidney's romance bot, Ahnaf's existence bot, or Ivy's therapy bot?";
+			questionType = "whichBot";
+		}
+		else
+		{
+			response = "Alright then, I guess this is bye.";
+			emotion++;
+		}
+		return response;
+	}
+	/*
+	 * Description: Creates a new bot for the user to talk to
+	 * Parameter: The user response represented by sentence
+	 * Return: Creates a new chatbot depending on what the answer inputted
+	 */
+	public void whichBotResponse(String sentence)
+	{
+		if(isKeywordFound(sentence, "Sidney's") || isKeywordFound(sentence, "Sidney") || isKeywordFound(sentence, "romance"))
+		{
+			ChatBotPhillips chatbot1 = new ChatBotPhillips();
+			System.out.println(chatbot1.getGreeting());
+			Scanner in = new Scanner (System.in);
+			String statement = in.nextLine();
+			
+			while (!statement.toLowerCase().equals("bye"))
+			{
+				System.out.println (chatbot1.getResponse(statement));
+				statement = in.nextLine();
+			}
+		}
+		if(isKeywordFound(sentence, "Ahnaf's") || isKeywordFound(sentence, "Ahnaf") || isKeywordFound(sentence, "existence"))
+		{
+			ChatBotAhnaf chatbot1 = new ChatBotAhnaf();
+			System.out.println(chatbot1.getGreeting());
+			Scanner in = new Scanner (System.in);
+			String statement = in.nextLine();
+			
+			while (!statement.toLowerCase().equals("bye"))
+			{
+				System.out.println (chatbot1.getResponse(statement));
+				statement = in.nextLine();
+			}
+		}
+		if(isKeywordFound(sentence, "Ivy's") || isKeywordFound(sentence, "Ivy") || isKeywordFound(sentence, "therapy"))
+		{
+			ChatBotGuan chatbot1 = new ChatBotGuan();
+			System.out.println(chatbot1.getGreeting());
+			Scanner in = new Scanner (System.in);
+			String statement = in.nextLine();
+			
+			while (!statement.toLowerCase().equals("bye"))
+			{
+				System.out.println (chatbot1.getResponse(statement));
+				statement = in.nextLine();
+			}
 		}
 	}
+	/*public boolean checkForBotKeywords(String statement, String [] switchBotWords)
+	{
+		for(int x=0; x<(switchBotWords.length); x++)
+		{
+			if(isKeywordFound(statement, switchBotWords[x]))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public String checkForWhichBot(String sentence)
+	{
+		if (checkForBotKeywords(sentence, romance))
+		{
+			String type = "";
+			type = "romance";
+			return type;
+		}
+		if (checkForBotKeywords(sentence, existence))
+		{
+			String type = "";
+			type = "existence";
+			return type;
+		}
+		if (checkForBotKeywords(sentence, therapy))
+		{
+			String type = "";
+			type = "therapy";
+			return type;
+		}
+		return "none";
+	}
+	public String checkForUserConsent(String type)
+	{
+		String response = "";
+		if(type == "romance")
+		{
+			response = "I see you mentioned something related to romance, would you like to talk to Sidney's bot about romance, relationships, and more?";
+			questionType = "changeBot";
+		}
+		if(type == "existence")
+		{
+			response = "I see you mentioned something related to existence, would you like to talk to Ahnaf's bot about space, time, and more?";
+			questionType = "changeBot";
+		}
+		if(type == "therapy")
+		{
+			response = "I see you mentioned something related to therapy, would you like to talk to Ivy's bot about therapy, help, and more?";
+			questionType = "changeBot";
+		}
+		if (type == "none")
+		{
+			
+		}
+		return response;
+	}
+	public void createNewChatBot(String userResponse, String contentType)
+	{
+		if(isKeywordFound(userResponse, "yes") || isKeywordFound(userResponse, "sure") || isKeywordFound(userResponse, "yea") || isKeywordFound(userResponse, "ye") || isKeywordFound(userResponse, "ok"))
+		{
+			if(isKeywordFound(contentType, "romance"))
+			{
+				ChatBotPhillips chatbot1 = new ChatBotPhillips();
+				System.out.println(chatbot1.getGreeting());
+				Scanner in = new Scanner (System.in);
+				String statement = in.nextLine();
+			}
+			if(isKeywordFound(contentType, "existence"))
+			{
+				ChatBotAhnaf chatbot1 = new ChatBotAhnaf();
+				System.out.println(chatbot1.getGreeting());
+				Scanner in = new Scanner (System.in);
+				String statement = in.nextLine();
+			}
+			if(isKeywordFound(contentType, "therapy"))
+			{
+				ChatBotGuan chatbot1 = new ChatBotGuan();
+				System.out.println(chatbot1.getGreeting());
+				Scanner in = new Scanner (System.in);
+				String statement = in.nextLine();
+			}
+		}
+		else
+		{
+			ChatBotZhang chatbot1 = new ChatBotZhang();
+			System.out.println(chatbot1.getGreeting());
+			Scanner in = new Scanner (System.in);
+			String statement = in.nextLine();
+		}
+	}
+	if(questionType == "changeBot")
+	{
+		if response is "" then function will not be able to find keyword then it will not create a new chatbot.
+		//if response has a keyword, it will create a new chatbot depending on the keyword found.
+		//if user replies with anything other than yes, yea, sure, ye, ok when asked if they wanted to switch, it will reset my bot and start the conversation over again
+		createNewChatBot(statement, runChatBotSwitch(statement));
+	}
+	*/
 }
